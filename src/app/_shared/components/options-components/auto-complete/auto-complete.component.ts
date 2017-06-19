425ff87a24b20c9aa074538a9d0b69559d5f6c5f
IsaacIsrael <isaacisrael37@gmail.com>
2017-06-19 11:35:06 -0300

    Improvement  on Auto Complete

--- src/app/_shared/components/options-components/auto-complete/auto-complete.component.ts
+++ src/app/_shared/components/options-components/auto-complete/auto-complete.component.ts
@@ -9,115 +9,92 @@ import{ OptionsComponent } from '../options-component';
   styleUrls: ['./auto-complete.component.css']
 })
 export class AutoCompleteComponent extends OptionsComponent  implements OnInit {
-  @Input()  placeholder:string = "";
-  @Input()  disabled: boolean = false;
-  @Output('keyword') keywordChange : EventEmitter<any> = new EventEmitter();
+  /******************* Inputs & Outputs ************************/
+    
+    @Input()  placeholder:string = "";
+    @Input()  disabled: boolean = false;
+    @Output('keyword') keywordChange : EventEmitter<any> = new EventEmitter();
   
-  private _keyword: string= "";
-  private isFocus:  boolean = false;
-  private list :Array<any> = [];
-
-  /*******************************************/
+  /******************** Fields ***********************/
   
-  set keyword(value: any){
-     this._keyword = this.transformOption(value);
-     this.list = this.Option(this._keyword);
+    private _keyword: string= "";
+    private isFocus:  boolean = false;
+    private list :Array<any> = [];
 
-     this.keywordChange.emit(this._keyword);
-  }
-  get keyword():any{
-    return this._keyword
-  }
-
-  protected  get FunctionOption():any{
-    return (value) => this.options(value);
-  }
-
-  protected  get ArrayOption():any{
-    return (value)=>  this.options.filter((item) => this.transformOption(item).toLocaleLowerCase().indexOf(value.toLocaleLowerCase()) != -1 ? true : false);
-  }
-
-  private get IsHideOptions():boolean{
-    return !this.isFocus || this.list.length <= 0;
-  }
-
-  private get IsSelectOptions():boolean{
-    return Array.isArray(this.options)
-  }
-
-  /*******************************************/
-
-  constructor() {
-    super();
-  }
-
-  ngOnInit(){
-      this.keyword = !this.multiSelect? this.value : "";
-      this.valueChange.subscribe((value)=>this.keyword = !this.multiSelect? value : "");
-  }
-
-  /*******************************************/
-
-   protected checkValue(value:any):boolean{
-
-      if(Array.isArray(value))
-        _.remove(value,(item)=>_.findIndex(this.Option(this.transformOption(item)),(option)=> _.isEqual(option,item)) == -1);
-        
-      return  _.isEmpty(value)
-              || Array.isArray(value) 
-              || _.findIndex(this.Option(this.transformOption(value)),(item)=> _.isEqual(value,item)) != -1;
-   }
-
-   private checkKeyword():void{
-
-      let validate = (item) =>_.isEqual(this.keyword,this.transformOption(item) );
-
-      if(_.findIndex(this.Option(this.transformOption(this.keyword)),(item)=>validate(item) ) != -1)
-        return;
-
-      let value = this.keyword;
-      this.clearValue();
-      this.keyword = value;
-   }
-
-   private isSelected(value):boolean{
-      return this.isValeu(value);
-   }
-
-  /*******************************************/
- 
-  private onFocusEventHandler(e:Event,arg:any):void{
-    this.isFocus = arg;
-
-    if(!this.multiSelect && !arg)
-      this.checkKeyword();  
-  }
-
-  private onKeyUpEventHandler(e:KeyboardEvent):void{
-      this.isFocus= true;
-      let target: any = e.target;
-         
-      setTimeout( ()=>this.keyword =target.value,500);
-  }
-
-  private onSelectOptionEventHandler(e:MouseEvent, args: any):void{
-      this.value = args;
-  }
-
-  private onClearEventHandler(e:MouseEvent):void{
-    if(!this.disabled){
-      this.clearValue();
+  /******************* Properties ************************/
+  
+    private set keyword(value: any){
+      this._keyword = this.transformOption(value);
+      this.list = this.Option(this._keyword);
+      this.keywordChange.emit(this._keyword);
     }
-  }
+    private get keyword():any{
+      return this._keyword
+    }
 
-  private onRemoveEventHandler(e:MouseEvent, args: any):void{
-    if(!this.disabled)
-      this.value = args;
-  }
-  
-  private onKeyDownBackspaceEventHandler(e:KeyboardEvent):void{
-      if(this.multiSelect && _.isEmpty(this.keyword))
-        this.value = _.last(this.value);
-  }
+    private get IsHideOptions():boolean{
+      return !this.isFocus || this.list.length <= 0;
+    }
 
+    private get IsSelectOptions():boolean{
+      return Array.isArray(this.options)
+    }
+
+    protected  get FunctionOption():any{
+      return (value) => this.options(this.transformOption(value));
+    }
+
+    protected  get ArrayOption():any{
+      return (value)=> this.options.filter((item) =>this.transformOption(item).toLocaleLowerCase().indexOf(this.transformOption(value).toLocaleLowerCase()) != -1 ? true : false );
+    }
+
+  /******************Constructor*************************/
+
+    constructor() {
+      super();
+    }
+
+    ngOnInit(){
+        this.keyword = !this.multiSelect? this.Value : "";
+        this.valueChange.subscribe((value)=>this.keyword = !this.multiSelect? value : "");
+    }
+
+  /***************** Methods**************************/
+
+    private isSelected(value):boolean{
+      return this.isValeu(value);
+    }
+
+  /******************Events*************************/
+ 
+    private onFocusEventHandler(e:Event,arg:any):void{
+      this.isFocus = arg;
+    }
+
+    private onKeyUpEventHandler(e:KeyboardEvent):void{
+        this.isFocus= true;
+        let target: any = e.target;
+          
+        setTimeout( ()=>this.keyword =target.value,500);
+    }
+
+    private onSelectOptionEventHandler(e:MouseEvent, args: any):void{
+        this.Value = args;
+    }
+
+    private onClearEventHandler(e:MouseEvent):void{
+      if(!this.disabled){
+        this.clearValue();
+      }
+    }
+
+    private onRemoveEventHandler(e:MouseEvent, args: any):void{
+      if(!this.disabled)
+        this.Value = args;
+    }
+    
+    private onKeyDownBackspaceEventHandler(e:KeyboardEvent):void{
+        if(this.multiSelect && _.isEmpty(this.keyword))
+          this.Value = _.last(this.Value);
+    }
 }
