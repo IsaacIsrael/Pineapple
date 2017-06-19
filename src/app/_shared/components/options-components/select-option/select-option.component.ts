669aef512bbe55fdd55d8a971075897e702d9def
IsaacIsrael <isaacisrael37@gmail.com>
2017-06-19 11:35:21 -0300

    Improvement  Select Options

--- src/app/_shared/components/options-components/select-option/select-option.component.ts
+++ src/app/_shared/components/options-components/select-option/select-option.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, HostListener, ElementRef } from '@angular/core';
+
+import { Slide } from '../../../animations/slideIn-slideOut.animation'
 import * as _ from "lodash";
 
 import{ OptionsComponent } from '../options-component';
@@ -6,24 +8,28 @@ import{ OptionsComponent } from '../options-component';
 @Component({
   selector: 'select-option',
   templateUrl: './select-option.component.html',
-  styleUrls: ['./select-option.component.css']
+  styleUrls: ['./select-option.component.css'],
+  animations: [ Slide(800,-15) ],
 })
 export class SelectOptionComponent extends OptionsComponent  {
+  /******************* Inputs & Outputs ************************/ 
+  
     @Input()  placeholder:string = "";
     @Input()  disabled: boolean = false;
+
+  /******************** Fields ***********************/
 
     private isFocus:  boolean = false;
     private list :Array<any> = [];
     
-    
-    /*******************************************/
+  /******************* Properties ************************/
 
     private get IsValueEmpty():boolean{
-        return  _.isEmpty(this.value);
+        return  _.isEmpty(this.Value);
     }
 
     private get Placeholder():string{
-      return this.IsValueEmpty ? this.placeholder : this.transformOption(this.value);
+      return this.IsValueEmpty ? this.placeholder : this.transformOption(this.Value);
     }
 
     private get isVisibelPlaceholder():boolean{
@@ -38,36 +44,29 @@ export class SelectOptionComponent extends OptionsComponent  {
       return (filter)=>this.options;
     }
 
-    /*******************************************/
+  /*******************Host Listeners************************/
 
     @HostListener('document:mousedown', ['$event'])
     onMouseDownHostListener(e: MouseEvent) {
       let target = e.srcElement || e.target;
-
-      if (!this.elementRef.nativeElement.contains(e.target))
+      
+      if ( this.isFocus && !this.elementRef.nativeElement.contains(e.target))
         this.isFocus = false;
     }
+  
+  /******************Constructor*************************/
 
     constructor(private elementRef:ElementRef) { 
       super();
     }
 
-    /*******************************************/
-
-    protected checkValue(value:any):boolean{
-      if(Array.isArray(value))
-       _.remove(value,(item)=>_.findIndex(this.Option(),(option)=> _.isEqual(option,item)) == -1);
-     
-      return  _.isEmpty(value) 
-            || Array.isArray(value) 
-            || _.findIndex(this.Option(),(item)=> _.isEqual(value,item)) != -1;
-    }
+  /***************** Methods**************************/
 
     private isSelected(value):boolean{
       return this.isValeu(value);
     }
 
-    /*******************************************/
+  /******************Events*************************/
 
     private onFocusEventHandler(e:Event,arg:any):void{
         if(!this.disabled){
@@ -82,13 +81,15 @@ export class SelectOptionComponent extends OptionsComponent  {
     }
 
     private onRemoveEventHandler(e:MouseEvent, args: any):void{
-       if(!this.disabled)
-          this.value = args;
+        if(!this.disabled)
+          this.Value = args;
     }
 
     private onSelectOptionEventHandler(e:MouseEvent, args: any):void{
-        this.value = args;
-        this.isFocus = false;
+        this.Value = args;
+        
+        if(!this.multiSelect)
+          this.isFocus =  false;
     }
 
 
