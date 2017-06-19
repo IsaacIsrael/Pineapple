309c3f37b293615116994a0d5341c708ed372d7e
IsaacIsrael <isaacisrael37@gmail.com>
2017-06-19 11:35:54 -0300

    Improvement on Animations

--- src/app/_shared/animations/slideIn-slideOut.animation.ts
+++ src/app/_shared/animations/slideIn-slideOut.animation.ts
@@ -22,17 +22,17 @@ export function Slide(duration: number = 300 , onflowY:number = -15) {
             transition('false => true', 
             [
                animate(duration + 'ms ease-in',keyframes([
-                    style({opacity: 0,transform: 'translateY(-100%)',height:'0', visibility:'visible', offset: 0}),
+                    style({opacity: 0, height:'0', offset: 0}),
                     style({opacity: 0, transform: 'translateY('+ onflowY +'px)',height:'*',  visibility:'visible', offset: 0.3}),
-                    style({opacity: 1, transform: 'translateY(0)', height:'*',    offset: 1.0})])
+                    style({opacity: 1, transform: 'translateY(0)', height:'*', offset: 1.0})])
                 )
             ]),
             transition('true => false', 
             [ 
                animate(duration + 'ms ease-in',keyframes([
-                    style({opacity: 1, transform: 'translateY(0)', height:'*',  visibility:'visible',   offset: 0}),
+                    style({opacity: 1, transform: 'translateY(0)', height:'*',  offset: 0}),
                     style({opacity: 0, transform: 'translateY('+ onflowY +'px)',height:'*', visibility:'visible', offset: 0.7}),
-                    style({opacity: 0, transform: 'translateY(-100%)',height:'0',  offset: 1.0})])
+                    style({opacity: 0, height:'0',  offset: 1.0})])
                 )
             ])
         ])
